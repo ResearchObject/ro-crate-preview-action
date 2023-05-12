5858a5ab0b1769446598dc1dc240e216fda9defc
Stian Soiland-Reyes <stain@apache.org>
2023-05-12 13:27:30 +0100

    Respect directory provided in symlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 rochtml $1/ro-crate-metadata.json
-ln -sf ro-crate-preview.html index.html
-echo "preview=ro-crate-preview.html" >> $GITHUB_OUTPUT
+ln -sf $1/ro-crate-preview.html $1/index.html
+echo "preview=$1/ro-crate-preview.html" >> $GITHUB_OUTPUT
